7cc3e218b7994abe2ec796fbc644734ae9fb9edf
erlon <erlon.opensrc@gmail.com>
2026-04-10 18:51:16 -0300

    Implementing CRUD routes

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,14 +2,56 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from todo.app import app
+
+def test_create_user(client: TestClient):
+    request_body = {
+        'username': 'Alice',
+        'email': 'alice@example.com',
+        'password': 'secret',
+    }
+
+    expected_response: dict[str, int | str] = {
+        'id': 1,
+        'email': 'alice@example.com',
+        'username': 'Alice',
+    }
+
+    response = client.post('/users/', json=request_body)
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == expected_response
 
 
-def test_root_deve_retorna_ok_e_hello_world():
-    client = TestClient(app) # Arrange -> Organizar
+def test_read_user(client: TestClient):
+    response = client.get('/users/')
 
-    response = client.get('/') # Act -> Agir
-
-    # Assert -> Afirmar
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Hello, World!'}
+    assert response.json() == {
+        'users': [{'id': 1, 'email': 'alice@example.com', 'username': 'Alice'}]
+    }
+
+
+def test_update_user(client: TestClient):
+    request_body = {
+        'username': 'Bob',
+        'email': 'bob@example.com',
+        'password': 'myNewPassword',
+    }
+
+    expected_response: dict[str, int | str] = {
+        'id': 1,
+        'email': 'bob@example.com',
+        'username': 'Bob',
+    }
+
+    response = client.put('/users/1', json=request_body)
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == expected_response
+
+
+def test_delete_user(client: TestClient):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
